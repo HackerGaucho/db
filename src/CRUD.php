2ffdb5b6fb55129b34179810f4e5982e3102ed6d
Anderson Ismael <aicoutos@users.noreply.github.com>
2017-09-20 16:17:50 -0300

    A  .project A  composer.json A  composer.lock A  src/CRUD.php

---
+++ src/CRUD.php
@@ -0,0 +1,26 @@
+<?php 
+/**
+ * User: Anderson Ismael
+ * Date: 20/set/2017
+ * IDE: Eclipse Oxygen 4.7.0
+ */
+
+namespace Basic;
+
+use Medoo\Medoo;
+
+class CRUD{
+        function __construct($db){
+            $this->db = new Medoo([
+                // required
+                'database_type' => 'mysql',
+                'database_name' => $db['name'],
+                'server' => $db['server'],
+                'username' => $db['user'],
+                'password' => $db['password'],
+                // [optional]
+                'charset' => 'utf8',
+                'port' => 3306
+            ]);
+        }
+}
